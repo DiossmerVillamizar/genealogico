2799c98f9f5c439666970429de147a1e7c6a3206
Diossmer <Diossmer@gmail.com>
2020-01-29 11:52:38 -0400

    session Parte 1

--- app/Http/Controllers/AdministradorController.php
+++ app/Http/Controllers/AdministradorController.php
@@ -10,7 +10,12 @@ class AdministradorController extends Controller
     public function __construct(){
         $this->middleware('EsAdmin');
     }
-    public function index(){
-        return " Si has llegado hasta aqui, eres administrador";
+    public function index(Request $request){
+        //return " Si has llegado hasta aqui, eres administrador";
+        //$request->session()->put(['Diossmer'=>'Administrador']);
+        //session(['Maria'=>'Estudiante']);
+        $request->session()->forget('Diossmer');$request->session()->flush();
+        return $request->session()->all();
+
     }
 }
