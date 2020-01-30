edd00320a2d71a87ee3696dfc04db9911eccd23e
Diossmer <Diossmer@gmail.com>
2020-01-30 11:48:29 -0400

    Correo Electronico SMTP

--- resources/views/mail/correo.blade.php
+++ resources/views/mail/correo.blade.php
@@ -12,11 +12,18 @@
     {!! Form::label("name", "Nombre", [null]) !!}<br>
     {!! Form::text("name", null, [null]) !!} <br>
     {!! Form::label("email", "Email", [null]) !!}<br>
-    {!! Form::text("email", null, [null]) !!} <br>
+    {!! Form::email("email", null, [null]) !!} <br>
     {!! Form::label("subject", "Asunto", [null]) !!}<br>
     {!! Form::text("subject", null, [null]) !!} <br>
     {!! Form::label("content", "Contenido", [null]) !!}<br>
     {!! Form::textarea("content", null, [null]) !!}<br>
+    {!! Form::submit("Enviado", [null]) !!}
+    {!! Form::close() !!}
+<br>
+    {!! Form::open(['url' => 'verifimail', "method"=>"post"]) !!}
+    {!! Form::token() !!}
+    {!! Form::label("mail", "Verificación Correo", [null]) !!}<br>
+    {!! Form::email("usermail", null, ["placeholder"=>"Email Address"]) !!} <br>
     {!! Form::submit("Enviado", [null]) !!}
     {!! Form::close() !!}
 </body>
