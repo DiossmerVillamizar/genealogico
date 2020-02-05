b33bd8959999ac06232669f7b65ec91f2976fd46
Diossmer <Diossmer@gmail.com>
2020-02-05 15:35:10 -0400

    morphMany Complete use (id)

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('morphMany',function(){
+    $post=\App\Post::find(1);
+    $video=\App\Video::find(1);
+    $comment=\App\Comment::find(1);
+
+    $post->comments()->create(['body'=>"NUEVO REQUEST"]);
+    $video->comments()->create(['body'=>"REQUEST request"]);
+
+    foreach ($post->comments as $comment) {
+        echo $comment->body."<br>";
+    }
+    foreach ($video->comments as $comment) {
+        echo $comment->body."<br>";
+    }
+    echo $comment->commentable->title;
+});
