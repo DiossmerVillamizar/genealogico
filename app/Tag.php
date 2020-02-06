0f9e1cf88048790aff50a796d22346a5f398f062
Diossmer <Diossmer@gmail.com>
2020-02-05 23:13:01 -0400

    morphedByMany complete

---
+++ app/Tag.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    //
+    protected $fillable=[
+        'name'
+    ];
+    /**
+    * Get all of the posts that are assigned this tag.
+    */
+    public function posts()
+    {
+        return $this->morphedByMany('App\Post', 'taggable');
+    }
+
+    /**
+    * Get all of the videos that are assigned this tag.
+    */
+    public function videos()
+    {
+        return $this->morphedByMany('App\Video', 'taggable');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('morphByMany/{id}', function ($id) {
+    // $post = \App\Post::create(["name"=>"NUEVO POST 1"]);
+    // $video = \App\Video::create(["name"=>"NUEVO VIDEOS 1"]);
+    // $tag = \App\Tag::create(["name"=>"NUEVO ETIQUETA 1"]);
+    $post = \App\Post::find($id);
+    $video = \App\Video::find($id);
+    $tag = \App\Tag::find($id);
+    // $tag->videos()->create(['name'=>"NUEVO Video 2"]);
+    // $tag->posts()->create(['name'=>"NUEVO Post 2"]);
+    // $post->tags()->create(['name'=>"NUEVO Tag 2"]);
+
+    foreach ($post->tags as $tag) {
+        echo $tag->name."<br>";
+    }
+    foreach ($tag->videos as $video) {
+        echo $video->name."<br>";
+    }
+    foreach ($tag->posts as $post) {
+        echo $post->name."<br>";
+    }
+    dd($tag->name,$video->name,$post->name);
+});
